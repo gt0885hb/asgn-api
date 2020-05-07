8211993258a5dd172f1ff89fe1594013e4d8b85a
unknown <59927775+gt0885hb@users.noreply.github.com>
2020-05-06 23:51:37 -0500

    npm modules set; Express server basically set; API entry point set; MongoDB connection set; body-parser module employed;

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
 let express = require('express')
 let app = express();
+let routes = require('./router');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 
 var port = 8080;
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+mongoose.connect('mongodb://localhost/asgn-api', {
+    useNewUrlParser: true
+})
+var db = mongoose.connection;
+
+if (!db) {
+    console.log("Connection unsuccessful!");
+} else {
+    console.log("Database connected successfully!");
+}
+
+app.use('/asgn-api', routes);
 
 app.get('/', (req, res) => res.send('Hello World with Express'));
 
